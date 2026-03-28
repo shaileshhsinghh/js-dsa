49a18c5dd56bbc971727f6abbb51516c3e8b9c18
Shailesh Singh <shailesh7360098753@gmail.com>
2026-03-28 12:22:11 +0530

    learned kth bit set or not

---
+++ kthbitset.js
@@ -0,0 +1,11 @@
+function kthbit(n,p){
+    let check = 1 ;
+    if((check & n) !== 0){
+        console.log(`Bit is set on position ${p}`)
+    }
+    else{
+        console.log(`Bit is not set on position ${p}`);
+    }
+}
+
+kthbit(6,1);
