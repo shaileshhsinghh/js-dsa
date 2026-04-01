972058feda7e9d47aaebef680519590c38bece6f
Shailesh Singh <shailesh7360098753@gmail.com>
2026-04-01 09:09:34 +0530

    made two odd acurring

---
+++ twooddoccur.js
@@ -0,0 +1,20 @@
+function twoadd(arr){
+    let xorAll = 0;
+    for(let n of arr){
+        xorAll ^= n;
+    }
+
+    let bit = xorAll & (-xorAll);
+    console.log(bit);
+
+    let x = 0 , y = 0;
+    for(n of arr){
+        if(n & bit) x ^= n;
+        else y ^= n;
+    }
+
+    return [x,y];
+}
+
+let arr = [1,3,2,4,5,6,6,2,1,3];
+console.log(twoadd(arr));
