4fe6b9089c527b187c3ad3f7fdcffc5bef67934c
Shailesh Singh <shailesh7360098753@gmail.com>
2026-04-07 09:47:53 +0530

    updating a new palindrone logic

--- palindronerec.js
+++ palindronerec.js
@@ -6,5 +6,11 @@ function palindrome(Original,rev = 0){
     return palindrome(Original,rev);
 }
 
-let input = 121;
-console.log(input === palindrome(input));
+function palindromeString(n, Original = n) {
+    if(n.length <= 1) return Original;                    
+    if(n[0] !== n[n.length-1]) return "Not a palindrome"; 
+    return palindromeString(n.slice(1, n.length-1), Original);
+}
+
+let input = "121";
+console.log(input === palindromeString(input));
